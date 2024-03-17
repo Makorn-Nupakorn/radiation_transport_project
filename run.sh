f6870077ee1bad2df46c31e47251e7ed34a92010
Makorn Nupakorn <makorn.nup@gmail.com>
2024-03-17 21:21:25 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-source env.venv/bin/activate
+brew install uv
+
+uv venv rt_env.venv
+
+source rt_env.venv/bin/activate
+
+uv pip install ipykernel rich pyyaml yml memory-profiler
 
 python3 main.py
